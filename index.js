cedfca53af8ec11ed2032cef2db59201603e70c0
Michael Hutchinson <75536318+AtlasR2D2@users.noreply.github.com>
2021-05-12 16:58:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+console.log("FIGHT!!!");
+console.log(superheroes.random()+" vs "+supervillains.random());
